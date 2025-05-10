3279ea83216321d89667f915f53f3ac130184d97
Vishnu Sarathy <vishnusarathy2006@gmail.com>
2025-05-10 23:29:39 +0530

    delete

--- CV/Photo.md
+++
@@ -1,1 +0,0 @@
-<img width="918" alt="image" src="https://github.com/user-attachments/assets/862704df-ea65-4631-bde8-d1d28354b951" />
--- ros_ass/src/py_pubsub/py_pubsub/open.py
+++
@@ -1,5 +0,0 @@
-import os 
-def main():
-    os.system("ros2 run py_pubsub talker & ros2 run py_pubsub listener")
-if __name__ == "__main__":
-    main()
